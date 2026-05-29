b3553b91cdf17e3b23f06fe34dba0f82dd1a017d
vullikantyabhishek9032 <vullikanty.abhishek@hcltech.com>
2026-05-29 16:21:33 +0530

    Frontend code

---
+++ Frontend/src/CommonComponents/CommonTable.js
@@ -0,0 +1,26 @@
+import Box from '@mui/material/Box';
+import { DataGrid } from '@mui/x-data-grid';
+
+
+export default function DataGridDemo({columns,rows}) {
+  return (
+    <Box sx={{ height: 400, width: '100%' }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        initialState={{
+          pagination: {
+            paginationModel: {
+              pageSize: 5,
+            },
+          },
+        }}
+        pageSizeOptions={[5]}
+        checkboxSelection
+        disableRowSelectionOnClick
+      />
+    </Box>
+  );
+}
+
+
