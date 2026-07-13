d390d0c4556947d6d9396d03651e33b9b40a2117
Sirasani Siva Krishna <sirasanisiva.krishna@hcltech.com>
2026-07-13 13:46:26 +0530

    Alignment changes

--- Kafka-Services/Consumer-Kafka-Service/src/main/java/com/hcl/Troy/Service/AlarmConsumerService.java
+++ Kafka-Services/Consumer-Kafka-Service/src/main/java/com/hcl/Troy/Service/AlarmConsumerService.java
@@ -20,20 +20,14 @@ public class AlarmConsumerService {
     private final List<TrapVarbindDTO> consumedTrapVarbinds = new ArrayList<>();
 
     private final List<RecommendationDTO> recommendations = new ArrayList<>();
-    
-    @KafkaListener(
-            topics = "metrics-topic",
-            groupId = "metrics-group"
-    )
+
+    @KafkaListener(topics = "metrics-topic", groupId = "metrics-group")
     public void consume(MonitoringEvent event) {
         consumedMetricsEvents.add(event);
         System.out.println("Metrics  Received : " + event);
     }
 
-    @KafkaListener(
-            topics = "alerts-topic",
-            groupId = "metrics-group"
-    )
+    @KafkaListener(topics = "alerts-topic", groupId = "metrics-group")
     public void consumeAlerts(AlertEvent event) {
         consumedAlertEvents.add(event);
         System.out.println("Alerts  Received : " + event);
@@ -41,19 +35,13 @@ public class AlarmConsumerService {
 
 
 
-    @KafkaListener(
-            topics = "snm-trap-topic",
-            groupId = "metrics-group"
-    )
+    @KafkaListener(topics = "snm-trap-topic", groupId = "metrics-group")
     public void consumeSnmpTraps(SnmpTrapDTO event) {
         consumedSnmpTraps.add(event);
         System.out.println("Snmp_traps  Received : " + event);
     }
 
-    @KafkaListener(
-            topics = "trap-varbinds-topic",
-            groupId = "metrics-group"
-    )
+    @KafkaListener(topics = "trap-varbinds-topic", groupId = "metrics-group")
     public void consumeSnmpTraps(TrapVarbindDTO event) {
         consumedTrapVarbinds.add(event);
         System.out.println("Trap_varbinds  Received : " + event);
