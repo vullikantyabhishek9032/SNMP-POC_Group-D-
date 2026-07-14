4431924b4fbce27664c18c2d7349cdc2201ae572
Sirasani Siva Krishna <sirasanisiva.krishna@hcltech.com>
2026-07-14 11:33:28 +0530

    Added crossorigin

--- Kafka-Services/Producer-kafka-service/src/main/java/com/hcl/Troy/Controller/AlarmController.java
+++ Kafka-Services/Producer-kafka-service/src/main/java/com/hcl/Troy/Controller/AlarmController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/events")
+@CrossOrigin(origins ="http://localhost:3000")
 public class AlarmController {
 
     @Autowired
