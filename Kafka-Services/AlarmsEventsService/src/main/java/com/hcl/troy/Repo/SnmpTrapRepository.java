086c9a174c245f88fe2b3a78472303bb72e449cd
Sirasani Siva Krishna <sirasanisiva.krishna@hcltech.com>
2026-07-15 12:33:18 +0530

    Alignment

--- Kafka-Services/AlarmsEventsService/src/main/java/com/hcl/troy/Repo/SnmpTrapRepository.java
+++ Kafka-Services/AlarmsEventsService/src/main/java/com/hcl/troy/Repo/SnmpTrapRepository.java
@@ -3,6 +3,5 @@ package com.hcl.troy.Repo;
 import com.hcl.troy.Entity.SnmpTrapEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SnmpTrapRepository
-        extends JpaRepository<SnmpTrapEntity, Long> {
+public interface SnmpTrapRepository extends JpaRepository<SnmpTrapEntity, Long> {
 }
--- Kafka-Services/AlarmsEventsService/src/main/java/com/hcl/troy/Repo/TrapVarbindRepository.java
+++ Kafka-Services/AlarmsEventsService/src/main/java/com/hcl/troy/Repo/TrapVarbindRepository.java
@@ -3,6 +3,5 @@ package com.hcl.troy.Repo;
 import com.hcl.troy.Entity.TrapVarbindEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TrapVarbindRepository
-        extends JpaRepository<TrapVarbindEntity, Long> {
+public interface TrapVarbindRepository extends JpaRepository<TrapVarbindEntity, Long> {
 }
