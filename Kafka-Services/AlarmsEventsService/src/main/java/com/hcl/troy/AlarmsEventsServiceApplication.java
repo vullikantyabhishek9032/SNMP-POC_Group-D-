c011bf947bf55439c4cbfcadd3ae712c91e2e9cd
Sirasani Siva Krishna <sirasanisiva.krishna@hcltech.com>
2026-07-15 12:11:02 +0530

    Updated

--- Kafka-Services/AlarmsEventsService/src/main/java/com/hcl/troy/AlarmsEventsServiceApplication.java
+++ Kafka-Services/AlarmsEventsService/src/main/java/com/hcl/troy/AlarmsEventsServiceApplication.java
@@ -10,7 +10,7 @@ public class AlarmsEventsServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AlarmsEventsServiceApplication.class, args);
-		System.out.println("New DB Connection Service");
+		System.out.println("AlarmEvents Monitoring Service");
 	}
 
 }
