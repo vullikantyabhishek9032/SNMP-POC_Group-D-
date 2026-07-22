4eeb3087f7bd1a46c851dcf307e048b8bcee244c
Sirasani Siva Krishna <sirasanisiva.krishna@hcltech.com>
2026-07-22 17:47:42 +0530

    Trap service testing

--- SNMPTrapMicroservice/src/main/java/com/example/snmptrapmicroservice/SnmpTrapMicroserviceApplication.java
+++ SNMPTrapMicroservice/src/main/java/com/example/snmptrapmicroservice/SnmpTrapMicroserviceApplication.java
@@ -12,7 +12,7 @@ public class SnmpTrapMicroserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SnmpTrapMicroserviceApplication.class, args);
-        System.out.println("SNMPTrap Service is Running ");
+        System.out.println("SNMPTrap Service is Running");
 	}
 
 }
