9b6bd907f0b9d07b4e0684676ea6a24c359ce847
Sirasani Siva Krishna <sirasanisiva.krishna@hcltech.com>
2026-07-22 18:08:48 +0530

    deployements check

--- Alert-read-service/src/main/java/com/poc/alert_read_service/AlertReadServiceApplication.java
+++ Alert-read-service/src/main/java/com/poc/alert_read_service/AlertReadServiceApplication.java
@@ -8,7 +8,10 @@ public class AlertReadServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AlertReadServiceApplication.class, args);
-        System.out.println("Alert Read Service Applcation is Running ");
+        System.out.println("Alert Read Service Applcation is Running");
 	}
 
 }
+
+
+
--- SNMPServices/SnmpMicroservice-1/src/main/java/com/example/snmpmicroservice/SnmpMicroservice1Application.java
+++ SNMPServices/SnmpMicroservice-1/src/main/java/com/example/snmpmicroservice/SnmpMicroservice1Application.java
@@ -10,6 +10,7 @@ public class SnmpMicroservice1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SnmpMicroservice1Application.class, args);
+        System.out.println("Snmp Microservice");
 	}
 
 }
