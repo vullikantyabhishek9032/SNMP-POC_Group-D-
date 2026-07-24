b2d2df40057b642d1e75dbdea141db4ab0a53afc
Sirasani Siva Krishna <sirasanisiva.krishna@hcltech.com>
2026-07-24 16:39:38 +0530

    Plan Mangement API

--- Frontend/src/Components/Report/index.js
+++ Frontend/src/Components/Report/index.js
@@ -1,23 +1,35 @@
+import { useEffect, useState } from "react";
 import { Styles } from "../../CommonComponents/Commonstyles"
 import DataGridDemo from "../../CommonComponents/CommonTable"
 import Searchinput from "../../CommonComponents/Searchinput"
+import { userDataUrl } from "../../Endpoint/endpoint";
 
 export default function Planlist() {
+    const [data, setData] = useState();
+    const response = async () => {
+        try {
+            const getData = await fetch(`${userDataUrl}/api/plans/fetechPlans`);
+            const APIData = await getData.json();
+            if (APIData) {
+                setData(APIData)
+            }
+        } catch (error) {
+            console.log("log the error response here", error);
+        }
+    }
+    useEffect(() => {
+        response();
+    }, []);
     const columns = [
         { field: "id", headerName: "ID", width: 90 },
         {
-            field: "plan_name",
+            field: "planName",
             headerName: "Plan Name",
             width: 150,
         },
         {
-            field: "data_limit",
+            field: "dataLimit",
             headerName: "Data Limit",
-            width: 150,
-        },
-        {
-            field: "plan_id",
-            headerName: "Current Plan",
             width: 150,
         },
         {
@@ -25,8 +37,12 @@ export default function Planlist() {
             headerName: "Benefits",
             width: 150,
         },
+        {
+            field: "nextPlan",
+            headerName: "Next_Plan",
+            width: 150,
+        },
     ];
-    const rows = []
     return (
         <>
             <div style={Styles.container}>
@@ -34,7 +50,11 @@ export default function Planlist() {
                     <Searchinput PlaceHolder={"Search by content"} />
                 </div>
                 <div style={Styles.tableContainer}>
-                    <DataGridDemo columns={columns} rows={rows} />
+                    <DataGridDemo columns={columns} rows={data?.map((data, index) => ({
+                        ...data,
+                        id: index + 1,
+                    }))} />
+
                 </div>
 
             </div>
