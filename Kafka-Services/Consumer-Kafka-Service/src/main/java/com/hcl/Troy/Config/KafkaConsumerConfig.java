92c7df8f20528ba3cc8eef8c4f38141f5dd413d0
Sirasani Siva Krishna <sirasanisiva.krishna@hcltech.com>
2026-07-24 11:06:51 +0530

    kafka Steams update

--- Kafka-Services/Consumer-Kafka-Service/src/main/java/com/hcl/Troy/Config/KafkaConsumerConfig.java
+++ Kafka-Services/Consumer-Kafka-Service/src/main/java/com/hcl/Troy/Config/KafkaConsumerConfig.java
@@ -1,22 +1,39 @@
 package com.hcl.Troy.Config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hcl.Troy.DTO.AlarmEvent;
 import com.hcl.Troy.DTO.RecommendationDTO;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.utils.Bytes;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.errors.StreamsUncaughtExceptionHandler;
+import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.state.KeyValueStore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.EnableKafkaStreams;
+import org.springframework.kafka.annotation.KafkaStreamsDefaultConfiguration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.KafkaStreamsConfiguration;
+import org.springframework.kafka.config.StreamsBuilderFactoryBeanConfigurer;
 import org.springframework.kafka.core.*;
 
 import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.kafka.support.serializer.JsonSerde;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
 @EnableKafka
+@EnableKafkaStreams
 public class KafkaConsumerConfig {
     @Bean
     public ConsumerFactory<String, AlarmEvent> consumerFactory() {
@@ -29,7 +46,7 @@ public class KafkaConsumerConfig {
         Map<String, Object> props = new HashMap<>();
 
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
-                "10.161.5.175:9092");
+                "localhost:9092");
 
         props.put(ConsumerConfig.GROUP_ID_CONFIG,
                 "alarm-group");
@@ -70,7 +87,7 @@ public class KafkaConsumerConfig {
 
         props.put(
                 ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
-                "10.161.5.175:9092");
+                "localhost:9092");
 
         props.put(
                 ConsumerConfig.GROUP_ID_CONFIG,
@@ -95,5 +112,91 @@ public class KafkaConsumerConfig {
         factory.setConsumerFactory(consumerFactoryRecommendation());
 
         return factory;
+    }
+
+
+
+    @Bean(name = KafkaStreamsDefaultConfiguration.DEFAULT_STREAMS_CONFIG_BEAN_NAME)
+    public KafkaStreamsConfiguration streamsConfig() {
+
+        Map<String, Object> props = new HashMap<>();
+
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "recommendation-streams");
+
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+
+        return new KafkaStreamsConfiguration(props);
+    }
+
+    @Bean
+    public KStream<String, RecommendationDTO> recommendationStream(StreamsBuilder builder) {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        return builder.stream("recommendation-topic",
+                        Consumed.with(
+                                Serdes.String(),
+                                Serdes.String()
+                        )
+                )
+                .mapValues(value -> {
+                    try {
+                        return objectMapper.readValue(value, RecommendationDTO.class);
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+    }
+
+
+
+
+    @Bean
+    public StreamsBuilderFactoryBeanConfigurer configurer() {
+        return factoryBean -> {
+            factoryBean.setStateListener((newState, oldState) -> {
+                System.out.println("STREAM STATE CHANGED: " + oldState + " -> " + newState);
+            });
+
+            factoryBean.setStreamsUncaughtExceptionHandler(
+                    exception -> {
+                        exception.printStackTrace();
+                        return StreamsUncaughtExceptionHandler.StreamThreadExceptionResponse.SHUTDOWN_APPLICATION;
+                    });
+        };
+    }
+
+
+    @Bean
+    public KTable<String, RecommendationDTO> recommendationTable(StreamsBuilder builder) {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        JsonSerde<RecommendationDTO> recommendationSerde = new JsonSerde<>(RecommendationDTO.class);
+
+        return builder
+                .stream(
+                        "recommendation-topic",
+                        Consumed.with(
+                                Serdes.String(),
+                                Serdes.String()
+                        )
+                )
+                .mapValues(value -> {
+                    try {
+                        return objectMapper.readValue(
+                                value,
+                                RecommendationDTO.class
+                        );
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                })
+                .selectKey((k, v) -> v.getCustomerId())
+                .toTable(Materialized.<String, RecommendationDTO, KeyValueStore<Bytes, byte[]>>as("recommendation-store")
+                                .withKeySerde(Serdes.String())
+                                .withValueSerde(recommendationSerde));
     }
 }
--- Kafka-Services/Consumer-Kafka-Service/src/main/java/com/hcl/Troy/Controller/ConsumerController.java
+++ Kafka-Services/Consumer-Kafka-Service/src/main/java/com/hcl/Troy/Controller/ConsumerController.java
@@ -2,8 +2,17 @@ package com.hcl.Troy.Controller;
 
 import com.hcl.Troy.DTO.*;
 import com.hcl.Troy.Service.AlarmConsumerService;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StoreQueryParameters;
+import org.apache.kafka.streams.state.KeyValueIterator;
+import org.apache.kafka.streams.state.QueryableStoreTypes;
+import org.apache.kafka.streams.state.ReadOnlyKeyValueStore;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.StreamsBuilderFactoryBean;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -12,7 +21,8 @@ import java.util.List;
 public class ConsumerController {
 
     private final AlarmConsumerService consumerService;
-
+    @Autowired
+    private StreamsBuilderFactoryBean factoryBean;
     public ConsumerController(
             AlarmConsumerService consumerService) {
 
@@ -33,9 +43,24 @@ public class ConsumerController {
     @GetMapping("/recommendations")
     public List<RecommendationDTO> getRecommendations() {
 
-        return consumerService.getRecommendations();
+        KafkaStreams streams=factoryBean.getKafkaStreams();
+        ReadOnlyKeyValueStore<String, RecommendationDTO> store =
+                streams.store(
+                        StoreQueryParameters.fromNameAndType(
+                                "recommendation-store",
+                                QueryableStoreTypes.keyValueStore()));
+
+        List<RecommendationDTO> list = new ArrayList<>();
+
+        KeyValueIterator<String, RecommendationDTO> iterator = store.all();
+
+        while(iterator.hasNext()){
+            list.add(iterator.next().value);
+        }
+
+        iterator.close();
+
+        return list;
+
     }
-
-
-
 }
